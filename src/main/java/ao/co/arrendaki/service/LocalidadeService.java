47d7af51491a0970ad80cd4ea2e0e56b167f3279
Hecroesmo <hecroesmo.jesus77@outlook.com>
2024-01-14 09:57:30 +0100

    Salvando e listando localidades

--- src/main/java/ao/co/arrendaki/service/LocalidadeService.java
+++ src/main/java/ao/co/arrendaki/service/LocalidadeService.java
@@ -27,29 +27,24 @@ public class LocalidadeService {
         return repositorio.save(localidade);
     }
 
-    public List<Localidade> buscarTodosPaises() {
-        return repositorio.findAllByLocalidadePaiIsNull();
-    }
+//    public List<Localidade> buscarTodosPaises() {
+//        return repositorio.findAllByLocalidadePaiIsNull();
+//    }
 
     public List<Localidade> buscarLocalidades() {
         return repositorio.findAll();
     }
 
-    public List<Localidade> buscarTodasLocalidadePeloChavePai(Long chave) {
-        return repositorio.findAllByLocalidadePaiPkLocalidade(chave);
-    }
+//    public List<Localidade> buscarTodasLocalidadePeloChavePai(Long chave) {
+//        return repositorio.findAllByLocalidadePaiPkLocalidade(chave);
+//    }
 
     public Localidade buscarPelaChave(Long chave) {
         Optional<Localidade> optional = repositorio.findById(chave);
         return optional.orElse(null);
     }
 
-    public Localidade deletarPelaChave(Long chave) {
-        Localidade localidade = buscarPelaChave(chave);
-        if (localidade != null) {
-            repositorio.delete(localidade);
-            return null;
-        }
-        return localidade;
+    public void deletarPelaChave(Long chave) {
+        repositorio.deleteById(chave);
     }
 }
